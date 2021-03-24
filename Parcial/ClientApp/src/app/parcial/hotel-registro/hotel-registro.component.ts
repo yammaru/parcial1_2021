e64153e14cd02ebcac9aa3e18d8fce8a1e815161
yammaru <63098405+yammaru@users.noreply.github.com>
2021-03-23 21:58:36 -0500

    7

--- Parcial/ClientApp/src/app/parcial/hotel-registro/hotel-registro.component.ts
+++ Parcial/ClientApp/src/app/parcial/hotel-registro/hotel-registro.component.ts
@@ -7,13 +7,26 @@ import { Hotel } from '../models/hotel';
   styleUrls: ['./hotel-registro.component.css']
 })
 export class HotelRegistroComponent implements OnInit {
-  persona: Hotel;
+  identificacion: Text;
+  nombre: Text;
+  apellido: Text;
+  hotel: Text;
+  fecha: Text;
+  tarifa: number;
+  persona: any;
   personasRegistradas: any;
   constructor() { }
 
   ngOnInit() {
   }
-  RH() {
+  add() {
+    this.persona.identificacion = this.identificacion;
+    this.persona.nombre = this.nombre;
+    this.persona.apellido = this.apellido;
+    this.persona.hotel = this.hotel;
+    this.persona.fecha = this.fecha;
+    this.persona.tarifa = this.calcularTarifa();
+
     this.personasRegistradas = this.obtenerPuls();
     this.personasRegistradas.push(this.persona);
 
@@ -32,15 +45,17 @@ export class HotelRegistroComponent implements OnInit {
     }
     return this.personasRegistradas;
   }
-calcularTarifa(){
-  if(this.calcularDia()=='A'){
-    if(this.persona.hotel=='HB'){
-      this.persona.tarifa=100000;
-    }if(this.persona.hotel=='HC'){this.persona.tarifa=85000;}else{this.persona.tarifa=95000;}
-  }else{if(this.persona.hotel=='HB'){
-    this.persona.tarifa=80000;
-  }if(this.persona.hotel=='HC'){this.persona.tarifa=78000;}else{this.persona.tarifa=75000;}}
-}
+  calcularTarifa() {
+    if (this.calcularDia() == 'A') {
+      if (this.persona.hotel == 'HB') {
+        this.persona.tarifa = 100000;
+      } if (this.persona.hotel == 'HC') { this.persona.tarifa = 85000; } else { this.persona.tarifa = 95000; }
+    } else {
+      if (this.persona.hotel == 'HB') {
+        this.persona.tarifa = 80000;
+      } if (this.persona.hotel == 'HC') { this.persona.tarifa = 78000; } else { this.persona.tarifa = 75000; }
+    }
+  }
   calcularDia() {
     var Xmas95 = new Date(this.persona.fecha);
     var weekday = Xmas95.getDay();
